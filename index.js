c9407c29f002d6f4b6e1dad65a66817f880be391
Violet Stanziani <stanzianiviolet@gmail.com>
2024-06-23 23:09:35 -0400

    started license section functions

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const questions = [
             type: "list",
             message:"What kind of license is being used?",
             name:"license",
-            choices: ["None","Apache License 2.0", "GNU General Public License v3.0","MIT License", "BSD 2-Clause'Simplified' License", "BSD 3-Clause 'New' or 'Revised' License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Ecplipse Public License 2.0", "GNU Affero General Public License v2.0","GNU Affero Genral Public License v3.0", "Mozzila Public License 2.0", "The Unilicense"]
+            choices: ["None","MIT","apache-2.0","gpl-3.0", "ISC"]
         },
         {
             type:"input",
