22c2c42d98b8cbafcbe3e50f551f4fdb15f8e967
Violet Stanziani <stanzianiviolet@gmail.com>
2024-06-23 23:14:07 -0400

    a little more function work

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -19,7 +19,11 @@ function renderLicenseLink(license) {
 // // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  
+  license = data.license
+  license==="None"
+  ? license = ''
+  : license = `This project uses a ${data.license} license.`
+  renderLicenseLink(data.license);
 }
 
 // TODO: Create a function to generate markdown for README
@@ -41,7 +45,6 @@ function generateMarkdown(data) {
   ${data.contribution} <br>
   ##Testing <br>
   ${data.testing} <br>
-  // renderLicenseSection(${data.license}); 
   ##Questions <br>
   If you have any questions please see my Github, https://github.com/${data.github} or feel free to reach out
   via email, ${data.email}. 
