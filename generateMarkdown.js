0db16c105ea1bd860d27cdf15d0f096f4416d9cd
Violet Stanziani <stanzianiviolet@gmail.com>
2024-06-23 21:35:59 -0400

    trying to figure out licenses

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,10 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
+const { link } = require("fs");
+
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  data.license ==="None"
+  ? license = ''
+  : license = 
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+ data.license==="None"
+ ? license = ''
+ : license = 
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,6 +23,20 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ##Description
+  ${data.description}
+  ##Installation
+  ${data.installation}
+  ##Usage
+  ${data.usage}
+  ##Contributors 
+  ${data.contribution}
+  ##Testing
+  ${data.testing}
+  renderLicenseSection(${data.license});
+  ##Questions
+  If you have any questions please see my Github, https://github.com/${username} or feel free to reach out
+  via email, ${email}.
 
 `;
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs')
-const genrateMarkdown = require ('./generateMarkdown')
+const generateMarkdown = require ('./generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = [
     inquirer 
@@ -9,7 +9,7 @@ const questions = [
         {
             type: "Input",
             message: "What's the name of your project?",
-            name: "project-title"
+            name: "title"
         },
         {
             type:"input",
@@ -29,7 +29,7 @@ const questions = [
         {
             type:"input",
             message:"Did you have any contributors?",
-            name:"contributions"
+            name:"contribution"
         },
         {
             type:"input",
@@ -41,6 +41,16 @@ const questions = [
             message:"What kind of license is being used?",
             name:"license",
             choices: ["None","Apache License 2.0", "GNU General Public License v3.0","MIT License", "BSD 2-Clause'Simplified' License", "BSD 3-Clause 'New' or 'Revised' License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Ecplipse Public License 2.0", "GNU Affero General Public License v2.0","GNU Affero Genral Public License v3.0", "Mozzila Public License 2.0", "The Unilicense"]
+        },
+        {
+            type:"input",
+            message:"What's your Github username?",
+            name:"github"
+        },
+        {
+            type:"input",
+            message:"What's your email address?",
+            name:"email"
         }
     ])
 ];
