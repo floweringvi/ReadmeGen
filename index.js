0fa25dd4f07c1d91492d446baefc1e6a7c8fc50f
Violet Stanziani <stanzianiviolet@gmail.com>
2024-06-23 21:09:57 -0400

    added choices of licenses

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const questions = [
             type: "list",
             message:"What kind of license is being used?",
             name:"license",
-            choices: []
+            choices: ["None","Apache License 2.0", "GNU General Public License v3.0","MIT License", "BSD 2-Clause'Simplified' License", "BSD 3-Clause 'New' or 'Revised' License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Ecplipse Public License 2.0", "GNU Affero General Public License v2.0","GNU Affero Genral Public License v3.0", "Mozzila Public License 2.0", "The Unilicense"]
         }
     ])
 ];
